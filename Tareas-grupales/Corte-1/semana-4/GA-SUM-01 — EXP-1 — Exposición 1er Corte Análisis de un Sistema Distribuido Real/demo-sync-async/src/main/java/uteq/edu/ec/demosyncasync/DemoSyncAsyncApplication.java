f4117567b9bbb980e9420d9008f2fe48d941276f
Freddy Farinango <freddy@gmail.com>
2026-05-25 23:07:09 -0500

    Organización de los archivos en cortes y semanas

---
+++ Tareas-grupales/Corte-1/semana-4/GA-SUM-01 — EXP-1 — Exposición 1er Corte Análisis de un Sistema Distribuido Real/demo-sync-async/src/main/java/uteq/edu/ec/demosyncasync/DemoSyncAsyncApplication.java
@@ -0,0 +1,13 @@
+package uteq.edu.ec.demosyncasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSyncAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSyncAsyncApplication.class, args);
+    }
+
+}
